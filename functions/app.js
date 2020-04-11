6e0890529ea6cc8b96b08bdf3df2b21a144c36ad
keisuke <rememberthatk@icloud.com>
2020-04-11 18:54:19 +0900

    [master] setup app

---
+++ functions/app.js
@@ -0,0 +1,72 @@
+// const admin = require('firebase-admin');
+// admin.initializeApp();
+// const express = require('express');
+// const cookieParser = require('cookie-parser')();
+// const cors = require('cors')({origin: true});
+// const app = express();
+//
+// const validateFirebaseIdToken = async (req, res, next) => {
+//   console.log('Check if request is authorized with Firebase ID token');
+//
+//   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+//     !(req.cookies && req.cookies.__session)) {
+//     console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+//       'Make sure you authorize your request by providing the following HTTP header:',
+//       'Authorization: Bearer <Firebase ID Token>',
+//       'or by passing a "__session" cookie.');
+//     res.status(403).send('Unauthorized');
+//     return;
+//   }
+//
+//   let idToken;
+//   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+//     console.log('Found "Authorization" header');
+//     idToken = req.headers.authorization.split('Bearer ')[1];
+//   } else if(req.cookies) {
+//     console.log('Found "__session" cookie');
+//     // Read the ID Token from cookie.
+//     idToken = req.cookies.__session;
+//   } else {
+//     // No cookie
+//     res.status(403).send('Unauthorized');
+//     return;
+//   }
+//
+//   try {
+//     const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+//     console.log('ID Token correctly decoded', decodedIdToken);
+//     req.user = decodedIdToken;
+//     next();
+//     return;
+//   } catch (error) {
+//     console.error('Error while verifying Firebase ID token:', error);
+//     res.status(403).send('Unauthorized');
+//     return;
+//   }
+// };
+//
+// app.use(cors);
+// app.use(cookieParser);
+// app.use(validateFirebaseIdToken);
+// app.get('/hello', (req, res) => {
+//   res.send(`Hello ${req.user.name}`);
+// });
+// app.post('/tweet', (req, res) => {
+//   const { word, accessKey, accessSecret } = req.body;
+//   cnsole.log('word', word);
+//   const client = twitter(accessKey, accessSecret);
+//
+//   client.post('statuses/update', {status: `this is ${word}`}, (error, tweet, response) => {
+//     if (!error) {
+//       res.send({ title: 'Express', tweet })
+//     }
+//     else {
+//       res.send({ error: "this is error: " + error })
+//     }
+//   });
+// });
+//
+// export default app;
+export const byeWorld = (request, response) => {
+  response.send("Hello from Firebase!");
+};
--- functions/index.js
+++ functions/index.js
@@ -4,31 +4,35 @@ const Twitter = require('twitter');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const config = functions.config();
+import { byeWorld } from './app.js';
 
-const twitter = (accessKey, accessSecrect) => {
-  return new Twitter({
-    consumer_key: config.twitter.consumer_key,
-    consumer_secret: config.twitter.consumer_secret,
-    access_token_key: accessKey,
-    access_token_secret: accessSecrect
-  });
-}
+// const config = functions.config();
+
+// const twitter = (accessKey, accessSecrect) => {
+//   return new Twitter({
+//     consumer_key: config.twitter.consumer_key,
+//     consumer_secret: config.twitter.consumer_secret,
+//     access_token_key: accessKey,
+//     access_token_secret: accessSecrect
+//   });
+// }
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-exports.tweet = functions.https.onRequest((req, res) => {
-  const { word, accessKey, accessSecret } = req.body;
-  const client = twitter(accessKey, accessSecret);
+exports.byeWorld = functions.https.onRequest(byeWorld);
 
-  client.post('statuses/update', {status: `this is ${word}`}, (error, tweet, response) => {
-    if (!error) {
-      res.send({ title: 'Express', tweet })
-    }
-    else {
-      res.send({ error: "this is error: " + error })
-    }
-  });
-});
+// exports.tweet = functions.https.onRequest((req, res) => {
+//   const { word, accessKey, accessSecret } = req.body;
+//   const client = twitter(accessKey, accessSecret);
+//
+//   client.post('statuses/update', {status: `this is ${word}`}, (error, tweet, response) => {
+//     if (!error) {
+//       res.send({ title: 'Express', tweet })
+//     }
+//     else {
+//       res.send({ error: "this is error: " + error })
+//     }
+//   });
+// });
